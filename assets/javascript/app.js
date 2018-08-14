ee6cee7e1c8486b3838d978df7d42a2edc1509a3
MBrennecke <MBrennecke10@gmail.com>
2018-08-14 15:25:39 -0600

    first commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBo9pOdrb0bLJ9Gs0pZ7KDPqpo48prIgko",
+    authDomain: "rock-paper-scissors-c7885.firebaseapp.com",
+    databaseURL: "https://rock-paper-scissors-c7885.firebaseio.com",
+    projectId: "rock-paper-scissors-c7885",
+    storageBucket: "rock-paper-scissors-c7885.appspot.com",
+    messagingSenderId: "282308550133"
+  };
+  firebase.initializeApp(config);
